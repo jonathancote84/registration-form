14f93cd999203fe393f50cbfa004f076325878ae
jonathancote84 <jonathancote92@gmail.com>
2019-05-15 15:26:29 -0700

    add name + password event

--- src/registrationForm/registrationForm.js
+++ src/registrationForm/registrationForm.js
@@ -6,36 +6,58 @@ import React, { Component } from 'react'
 class RegistrationForm extends Component {
     constructor(props) {
         super(props);
-        this.nameInput = React.createRef();
+        this.state = {
+            name: '',
+            password: '',
+            repeatPassword: ''
+        }
     }
+    updateName(name) {
+        this.setState({name});
+    }
+
+    updatePassword(password) {
+        this.setState({password});
+    }
+
+    updateRepeatPassword(repeatPassword) {
+        this.setState({repeatPassword});
+    }
+
     handleSubmit(event) {
         event.preventDefault();
-        const name = this.nameInput.current.value;
+        const { name, password, repeatPassword } = this.state;
+
         console.log('Name: ', name);
-        //process form values here
+        console.log('Password: ', password);
+        console.log('Repeat Password: ', repeatPassword);
+
+        //potentially submit these values to the server here
     }
+
+
 
     render () {
         
         return (
-        <form className="registration" onSubmit={e=> this.handleSubmit(e)}>
+        <form className="registration" onSubmit={e => this.handleSubmit(e)}>
             <h2>Register</h2>
             <div className="registration__hint">* required field</div>  
             <div className="form-group">
             <label htmlFor="name">Name *</label>
             <input type="text" className="registration__control"
-                name="name" id="name" ref={this.nameInput} defaultValue="Frank"/>
+                name="name" id="name" onChange={e => this.updateName(e.target.value)} />
             </div>
             <div className="form-group">
             <label htmlFor="password">Password *</label>
             <input type="password" className="registration__control"
-                name="password" id="password"/>
+                name="password" id="password" onChange={e => this.updatePassword(e.target.value)}/>
             <div className="registration__hint">6 to 72 characters, must include a number</div>
             </div>
             <div className="form-group">
             <label htmlFor="repeatPassword">Repeat Password *</label>
             <input type="password" className="registration__control"
-                name="repeatPassword" id="repeatPassword"/>
+                name="repeatPassword" id="repeatPassword" onChange={e => this.updateRepeatPassword(e.target.value)}/>
             </div>
     
             <div className="registration__button__group">
