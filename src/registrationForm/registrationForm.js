cb6c19f1c8988fbc67c32e60995b0c0d9d944694
jonathancote84 <jonathancote92@gmail.com>
2019-05-17 09:53:13 -0700

    form validation react

--- src/registrationForm/registrationForm.js
+++ src/registrationForm/registrationForm.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react'
+import ValidationError from '../ValidationError/ValidationError'
 
 
 
 
 class RegistrationForm extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             name: '',
             password: '',
-            repeatPassword: ''
+            repeatPassword: '',
+            nameValid: false,
+            passwordValid: false,
+            passwordMatch: false,
+            formValid: false,
+            validationMessages: {
+                name: '',
+                password: '',
+                repeatPassword: '',
+            }
         }
     }
+
     updateName(name) {
-        this.setState({name});
+        this.setState({name}, () => {this.validateName(name)});
     }
 
     updatePassword(password) {
-        this.setState({password});
+        this.setState({password}, () => {this.validatePassword(password)});
     }
 
     updateRepeatPassword(repeatPassword) {
-        this.setState({repeatPassword});
+        this.setState({repeatPassword}, () => {this.matchPasswords(repeatPassword)});
     }
 
     handleSubmit(event) {
@@ -35,7 +47,85 @@ class RegistrationForm extends Component {
         //potentially submit these values to the server here
     }
 
+    validateName(fieldValue) {
+        const fieldErrors = {...this.state.validationMessages};
+        let hasError = false;
 
+        fieldValue = fieldValue.trim();
+        if(fieldValue.length === 0) {
+            fieldErrors.name = 'Name is required';
+            hasError = true;
+        } else {
+            if (fieldValue.length < 3) {
+                fieldErrors.name = 'Name must be at least 3 characters long';
+                hasError = true;
+            } else {
+                fieldErrors.name = '';
+                hasError = false;  
+            }           
+        }
+
+        this.setState({
+            validationMessages: fieldErrors,
+            nameValid: !hasError
+        }, this.formValid );
+    }
+
+    validatePassword(fieldValue) {
+        const fieldErrors = {...this.state.validationMessages};
+        let hasError = false;
+
+        fieldValue = fieldValue.trim();
+        if(fieldValue.length === 0) {
+            fieldErrors.password = 'Password is required';
+            hasError = true;
+        } else {
+            if (fieldValue.length < 6 || fieldValue.length > 72) {
+               fieldErrors.password = 'Password must be between 6 and 72 characters long';
+               hasError = true;
+            } else {
+                if(!fieldValue.match(new RegExp(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/))) {
+                    fieldErrors.password = 'Password must contain at least one number and one letter';
+                    hasError = true;
+                } else {
+                    fieldErrors.password = '';
+                    hasError = false;
+                }
+            }
+        }
+
+        this.setState({
+            validationMessages: fieldErrors,
+            passwordValid: !hasError
+        }, this.formValid );
+
+    }
+
+    matchPasswords(fieldValue) {
+        const fieldErrors = {...this.state.validationMessages};
+        let hasError = false;
+
+        const password = this.state.password;
+
+        if(fieldValue !== password) {
+            fieldErrors.repeatPassword = 'Passwords do not match';
+            hasError = true
+        } else {
+            fieldErrors.repeatPassword = '';
+            hasError = false;
+        }
+        
+        this.setState({
+            validationMessages: fieldErrors,
+            passwordMatch: !hasError
+        }, this.formValid );
+    }
+
+    formValid() {
+        this.setState({
+            formValid: this.state.nameValid && this.state.passwordValid && this.state.passwordMatch
+        });
+    }
 
     render () {
         
@@ -47,24 +137,27 @@ class RegistrationForm extends Component {
             <label htmlFor="name">Name *</label>
             <input type="text" className="registration__control"
                 name="name" id="name" onChange={e => this.updateName(e.target.value)} />
+            <ValidationError hasError={!this.state.nameValid} message={this.state.validationMessages.name}/>
             </div>
             <div className="form-group">
             <label htmlFor="password">Password *</label>
             <input type="password" className="registration__control"
                 name="password" id="password" onChange={e => this.updatePassword(e.target.value)}/>
+            <ValidationError hasError={!this.state.nameValid} message={this.state.validationMessages.password}/>
             <div className="registration__hint">6 to 72 characters, must include a number</div>
             </div>
             <div className="form-group">
             <label htmlFor="repeatPassword">Repeat Password *</label>
             <input type="password" className="registration__control"
                 name="repeatPassword" id="repeatPassword" onChange={e => this.updateRepeatPassword(e.target.value)}/>
+            <ValidationError hasError={!this.state.nameValid} message={this.state.validationMessages.repeatPassword}/>
             </div>
     
             <div className="registration__button__group">
             <button type="reset" className="registration__button">
                 Cancel
             </button>
-            <button type="submit" className="registration__button">
+            <button type="submit" className="registration__button" disabled={!this.state.formValid}>
                 Save
             </button>
             </div>
